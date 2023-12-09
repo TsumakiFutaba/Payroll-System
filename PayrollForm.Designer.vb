a2a666ebe25d091d554495673fa9d5e471b036ee
Tsumaki <matsu.yukii22@gmail.com>
2023-12-10 06:49:04 +0800

    added logic on the payroll form and added columns basigwage on Employee_Info and totalHours on Employee_Attendances at the database

--- PayrollForm.Designer.vb
+++ PayrollForm.Designer.vb
@@ -26,30 +26,23 @@ Partial Class PayrollForm
         pnlDGVPayroll = New Panel()
         DGVPayroll = New DataGridView()
         pnlPayrollInformation = New Panel()
-        ComboBox1 = New ComboBox()
         lblPagIbigValue = New Label()
         lblPhilHealthValue = New Label()
         lblSssValue = New Label()
-        lblOvertimePayValue = New Label()
         lblBasicSalaryValue = New Label()
         lblNameValue = New Label()
         btnSearch = New Button()
         tbSearchBar = New TextBox()
         lblSearchEmployee = New Label()
-        lblOvertimePay = New Label()
         lblPagIbig = New Label()
         lblPhilHealth = New Label()
         lblSss = New Label()
         lblDeductions = New Label()
-        Label5 = New Label()
         lblBasicSalary = New Label()
         lblEmployeeName = New Label()
         lblSalaryScheme = New Label()
         lblPayroll = New Label()
         btnPaySlip = New Button()
-        Column1 = New DataGridViewTextBoxColumn()
-        Column2 = New DataGridViewTextBoxColumn()
-        Column3 = New DataGridViewTextBoxColumn()
         Column4 = New DataGridViewTextBoxColumn()
         Column5 = New DataGridViewTextBoxColumn()
         Column6 = New DataGridViewTextBoxColumn()
@@ -84,7 +77,7 @@ Partial Class PayrollForm
         DGVPayroll.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
         DGVPayroll.BackgroundColor = SystemColors.ControlLightLight
         DGVPayroll.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DGVPayroll.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4, Column5, Column6, Column8})
+        DGVPayroll.Columns.AddRange(New DataGridViewColumn() {Column4, Column5, Column6, Column8})
         DGVPayroll.Dock = DockStyle.Fill
         DGVPayroll.Location = New Point(0, 0)
         DGVPayroll.Name = "DGVPayroll"
@@ -96,22 +89,18 @@ Partial Class PayrollForm
         ' pnlPayrollInformation
         ' 
         pnlPayrollInformation.BackColor = SystemColors.ControlLightLight
-        pnlPayrollInformation.Controls.Add(ComboBox1)
         pnlPayrollInformation.Controls.Add(lblPagIbigValue)
         pnlPayrollInformation.Controls.Add(lblPhilHealthValue)
         pnlPayrollInformation.Controls.Add(lblSssValue)
-        pnlPayrollInformation.Controls.Add(lblOvertimePayValue)
         pnlPayrollInformation.Controls.Add(lblBasicSalaryValue)
         pnlPayrollInformation.Controls.Add(lblNameValue)
         pnlPayrollInformation.Controls.Add(btnSearch)
         pnlPayrollInformation.Controls.Add(tbSearchBar)
         pnlPayrollInformation.Controls.Add(lblSearchEmployee)
-        pnlPayrollInformation.Controls.Add(lblOvertimePay)
         pnlPayrollInformation.Controls.Add(lblPagIbig)
         pnlPayrollInformation.Controls.Add(lblPhilHealth)
         pnlPayrollInformation.Controls.Add(lblSss)
         pnlPayrollInformation.Controls.Add(lblDeductions)
-        pnlPayrollInformation.Controls.Add(Label5)
         pnlPayrollInformation.Controls.Add(lblBasicSalary)
         pnlPayrollInformation.Controls.Add(lblEmployeeName)
         pnlPayrollInformation.Controls.Add(lblSalaryScheme)
@@ -121,17 +110,6 @@ Partial Class PayrollForm
         pnlPayrollInformation.Size = New Size(997, 327)
         pnlPayrollInformation.TabIndex = 0
         ' 
-        ' ComboBox1
-        ' 
-        ComboBox1.BackColor = SystemColors.InactiveCaption
-        ComboBox1.FlatStyle = FlatStyle.Flat
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
-        ComboBox1.Location = New Point(755, 134)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(197, 23)
-        ComboBox1.TabIndex = 22
-        ' 
         ' lblPagIbigValue
         ' 
         lblPagIbigValue.AutoSize = True
@@ -139,9 +117,8 @@ Partial Class PayrollForm
         lblPagIbigValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
         lblPagIbigValue.Location = New Point(488, 263)
         lblPagIbigValue.Name = "lblPagIbigValue"
-        lblPagIbigValue.Size = New Size(42, 25)
+        lblPagIbigValue.Size = New Size(0, 25)
         lblPagIbigValue.TabIndex = 19
-        lblPagIbigValue.Text = "245"
         ' 
         ' lblPhilHealthValue
         ' 
@@ -150,9 +127,8 @@ Partial Class PayrollForm
         lblPhilHealthValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
         lblPhilHealthValue.Location = New Point(341, 264)
         lblPhilHealthValue.Name = "lblPhilHealthValue"
-        lblPhilHealthValue.Size = New Size(42, 25)
+        lblPhilHealthValue.Size = New Size(0, 25)
         lblPhilHealthValue.TabIndex = 18
-        lblPhilHealthValue.Text = "245"
         ' 
         ' lblSssValue
         ' 
@@ -161,31 +137,18 @@ Partial Class PayrollForm
         lblSssValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
         lblSssValue.Location = New Point(224, 264)
         lblSssValue.Name = "lblSssValue"
-        lblSssValue.Size = New Size(42, 25)
+        lblSssValue.Size = New Size(0, 25)
         lblSssValue.TabIndex = 17
-        lblSssValue.Text = "900"
-        ' 
-        ' lblOvertimePayValue
-        ' 
-        lblOvertimePayValue.AutoSize = True
-        lblOvertimePayValue.Font = New Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point)
-        lblOvertimePayValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
-        lblOvertimePayValue.Location = New Point(224, 207)
-        lblOvertimePayValue.Name = "lblOvertimePayValue"
-        lblOvertimePayValue.Size = New Size(42, 25)
-        lblOvertimePayValue.TabIndex = 16
-        lblOvertimePayValue.Text = "200"
         ' 
         ' lblBasicSalaryValue
         ' 
         lblBasicSalaryValue.AutoSize = True
         lblBasicSalaryValue.Font = New Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point)
         lblBasicSalaryValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
-        lblBasicSalaryValue.Location = New Point(224, 170)
+        lblBasicSalaryValue.Location = New Point(224, 174)
         lblBasicSalaryValue.Name = "lblBasicSalaryValue"
-        lblBasicSalaryValue.Size = New Size(66, 25)
+        lblBasicSalaryValue.Size = New Size(0, 25)
         lblBasicSalaryValue.TabIndex = 15
-        lblBasicSalaryValue.Text = "30,000"
         ' 
         ' lblNameValue
         ' 
@@ -194,9 +157,8 @@ Partial Class PayrollForm
         lblNameValue.ForeColor = Color.FromArgb(CByte(56), CByte(59), CByte(62))
         lblNameValue.Location = New Point(224, 125)
         lblNameValue.Name = "lblNameValue"
-        lblNameValue.Size = New Size(91, 25)
+        lblNameValue.Size = New Size(0, 25)
         lblNameValue.TabIndex = 14
-        lblNameValue.Text = "John Doe"
         ' 
         ' btnSearch
         ' 
@@ -224,21 +186,9 @@ Partial Class PayrollForm
         lblSearchEmployee.ForeColor = SystemColors.ControlDarkDark
         lblSearchEmployee.Location = New Point(247, 71)
         lblSearchEmployee.Name = "lblSearchEmployee"
-        lblSearchEmployee.Size = New Size(136, 15)
+        lblSearchEmployee.Size = New Size(114, 15)
         lblSearchEmployee.TabIndex = 11
-        lblSearchEmployee.Text = "Search Employee name :"
-        ' 
-        ' lblOvertimePay
-        ' 
-        lblOvertimePay.AutoSize = True
-        lblOvertimePay.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
-        lblOvertimePay.ForeColor = SystemColors.ControlDarkDark
-        lblOvertimePay.Location = New Point(93, 206)
-        lblOvertimePay.Name = "lblOvertimePay"
-        lblOvertimePay.Size = New Size(109, 19)
-        lblOvertimePay.TabIndex = 10
-        lblOvertimePay.Text = "Overtime Pay :"
-        lblOvertimePay.TextAlign = ContentAlignment.MiddleLeft
+        lblSearchEmployee.Text = "Search Employee ID:"
         ' 
         ' lblPagIbig
         ' 
@@ -288,28 +238,16 @@ Partial Class PayrollForm
         lblDeductions.Text = "Deductions :"
         lblDeductions.TextAlign = ContentAlignment.MiddleLeft
         ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
-        Label5.ForeColor = SystemColors.ControlDarkDark
-        Label5.Location = New Point(699, 134)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(48, 19)
-        Label5.TabIndex = 3
-        Label5.Text = "Date :"
-        Label5.TextAlign = ContentAlignment.MiddleLeft
-        ' 
         ' lblBasicSalary
         ' 
         lblBasicSalary.AutoSize = True
         lblBasicSalary.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point)
         lblBasicSalary.ForeColor = SystemColors.ControlDarkDark
-        lblBasicSalary.Location = New Point(108, 169)
+        lblBasicSalary.Location = New Point(63, 176)
         lblBasicSalary.Name = "lblBasicSalary"
-        lblBasicSalary.Size = New Size(98, 19)
+        lblBasicSalary.Size = New Size(143, 19)
         lblBasicSalary.TabIndex = 2
-        lblBasicSalary.Text = "Basic Salary :"
+        lblBasicSalary.Text = "Basic Hourly Wage :"
         ' 
         ' lblEmployeeName
         ' 
@@ -356,24 +294,6 @@ Partial Class PayrollForm
         btnPaySlip.TabIndex = 2
         btnPaySlip.Text = "Payslip"
         btnPaySlip.UseVisualStyleBackColor = False
-        ' 
-        ' Column1
-        ' 
-        Column1.HeaderText = "Date"
-        Column1.Name = "Column1"
-        Column1.ReadOnly = True
-        ' 
-        ' Column2
-        ' 
-        Column2.HeaderText = "Time In"
-        Column2.Name = "Column2"
-        Column2.ReadOnly = True
-        ' 
-        ' Column3
-        ' 
-        Column3.HeaderText = "Time Out"
-        Column3.Name = "Column3"
-        Column3.ReadOnly = True
         ' 
         ' Column4
         ' 
@@ -427,8 +347,6 @@ Partial Class PayrollForm
     Friend WithEvents lblEmployeeName As Label
     Friend WithEvents lblSalaryScheme As Label
     Friend WithEvents lblDeductions As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents lblOvertimePay As Label
     Friend WithEvents lblPagIbig As Label
     Friend WithEvents lblPhilHealth As Label
     Friend WithEvents lblSss As Label
@@ -438,15 +356,10 @@ Partial Class PayrollForm
     Friend WithEvents tbSearchBar As TextBox
     Friend WithEvents lblNameValue As Label
     Friend WithEvents lblSssValue As Label
-    Friend WithEvents lblOvertimePayValue As Label
     Friend WithEvents lblBasicSalaryValue As Label
     Friend WithEvents lblPagIbigValue As Label
     Friend WithEvents lblPhilHealthValue As Label
-    Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents btnPaySlip As Button
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents Column5 As DataGridViewTextBoxColumn
     Friend WithEvents Column6 As DataGridViewTextBoxColumn
--- PayrollForm.vb
+++ PayrollForm.vb
@@ -1,7 +1,139 @@
-﻿Public Class PayrollForm
+﻿Imports System.Threading
+Imports System.Data.SqlClient
+
+Public Class PayrollForm
+    Dim engine = "localhost, 1433"
+    Dim db = "payrolldatabase"
+    Dim conn As String = ($"Data Source={engine};Initial Catalog={db};Integrated Security=true")
+
     Private Sub btnPaySlip_Click(sender As Object, e As EventArgs) Handles btnPaySlip.Click
         Dim frm As New PayslipForm
         frm.Location = New Point((WorkingArea.Width - frm.Width) / 2, 0)
         TransparentBG.ShowForm(frm)
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        SearchID()
+
+    End Sub
+    Public Sub SearchID()
+        Dim ID As Integer = Integer.Parse(tbSearchBar.Text)
+        Dim USERNAME As String = lblNameValue.Text
+        Dim query As String = "SELECT * FROM Employee_Info WHERE employee_id=@employee_id"
+        Dim totalQuery As String = "SELECT SUM(totalHours) AS TotalSum FROM Employee_Attendances WHERE USERNAME=@USERNAME"
+        Dim deductionQuery As String = "SELECT (ISNULL(sss, 0) + ISNULL(philhealth, 0) + ISNULL(pagibig, 0)) AS TotalSum FROM Employee_Info WHERE usrname=@USERNAME"
+
+
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+
+
+            Using cmd As New SqlCommand(query, connection)
+                cmd.Parameters.AddWithValue("@employee_id", ID)
+
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        Dim name As String = reader("employee_name").ToString
+                        Dim SSS As Integer = reader("sss").ToString
+                        Dim Philhealth As Integer = reader("philhealth").ToString
+                        Dim PagIbig As Integer = reader("pagibig").ToString
+                        Dim BasicHourlyWage As Integer = reader("basicwage").ToString
+                        lblNameValue.Text = name.ToUpper
+                        lblSssValue.Text = SSS
+                        lblPhilHealthValue.Text = Philhealth
+                        lblPagIbigValue.Text = PagIbig
+                        lblBasicSalaryValue.Text = BasicHourlyWage
+
+
+                    Else
+
+                        MessageBox.Show($"User with ID {ID} not found.")
+                    End If
+                End Using
+            End Using
+        End Using
+
+
+        'Total Hours
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+
+            Using cmd As New SqlCommand(totalQuery, connection)
+                cmd.Parameters.AddWithValue("@USERNAME", lblNameValue.Text)
+
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        Dim totalSum As Integer = If(reader.IsDBNull(0), 0, reader.GetInt32(0))
+                        If DGVPayroll.Rows.Count > 0 AndAlso DGVPayroll.Columns.Count > 0 Then
+                            DGVPayroll.Rows(0).Cells(0).Value = totalSum
+                        Else
+
+                        End If
+                    End If
+                End Using
+            End Using
+        End Using
+
+        'Wages
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+
+            Using cmd As New SqlCommand(totalQuery, connection)
+                cmd.Parameters.AddWithValue("@USERNAME", lblNameValue.Text)
+
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        Dim totalSum As Integer = If(reader.IsDBNull(0), 0, reader.GetInt32(0))
+                        Dim basicsalary As Integer = Convert.ToInt32(lblBasicSalaryValue.Text)
+                        If DGVPayroll.Rows.Count > 0 AndAlso DGVPayroll.Columns.Count > 0 Then
+                            DGVPayroll.Rows(0).Cells(1).Value = totalSum * basicsalary
+                        Else
+
+                        End If
+                    End If
+                End Using
+            End Using
+        End Using
+
+        'Total Deductions
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+
+            Using cmd As New SqlCommand(deductionQuery, connection)
+                cmd.Parameters.AddWithValue("@USERNAME", lblNameValue.Text)
+
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        Dim totalSum As Integer = If(reader.IsDBNull(0), 0, reader.GetInt32(0))
+                        If DGVPayroll.Rows.Count > 0 AndAlso DGVPayroll.Columns.Count > 0 Then
+                            DGVPayroll.Rows(0).Cells(2).Value = totalSum
+                        Else
+
+                        End If
+                    End If
+                End Using
+            End Using
+        End Using
+
+        'Net Wages
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+
+            Using cmd As New SqlCommand(deductionQuery, connection)
+                cmd.Parameters.AddWithValue("@USERNAME", lblNameValue.Text)
+
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        Dim totalSum As Integer = If(reader.IsDBNull(0), 0, reader.GetInt32(0))
+                        Dim Wages As Integer = Convert.ToInt32(DGVPayroll.Rows(0).Cells(1).Value)
+                        If DGVPayroll.Rows.Count > 0 AndAlso DGVPayroll.Columns.Count > 0 Then
+                            DGVPayroll.Rows(0).Cells(3).Value = Wages - totalSum
+                        Else
+
+                        End If
+                    End If
+                End Using
+            End Using
+        End Using
     End Sub
 End Class
